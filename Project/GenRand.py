e01c47e64fa207004872451d27b9d4a565824e16
Kayky_CN <84750720+Kcioban@users.noreply.github.com>
2023-08-07 11:49:45 -0300

    Update GenRand.py

--- Project/GenRand.py
+++ Project/GenRand.py
@@ -1,6 +1,10 @@
 # Geração de numero aleatório
 
+# Import da biblioteca de randomização
 import random
 
+# Definindo o limite que o rand irá
 numero_aleatorio = random.randint(0,100)
+
+# Plot do numero atribuido na variavel
 print('O número gerado é', numero_aleatorio)
