8065505cf6762217a1f7996bf301d86d257adc2b
Kayky_CN <84750720+Kcioban@users.noreply.github.com>
2023-09-05 11:13:02 -0300

    Update ContRegress.py
    rework do codigo

--- Project/ContRegress.py
+++ Project/ContRegress.py
@@ -1,4 +1,8 @@
-num = input("digite um numero para realizar a contagem regressiva dele: ")
+# inserção do numero.
+num = int(input("Digite um número para realizar a contagem regressiva dele: "))
 
-print("numero atual: " + num)
-while num > 0;
+print("Número atual: " + str(num))  # Convertendo num para string para poder concatená-lo
+
+while num > 0:   # Enquanto não zerar ele decresce de 1 em 1
+    print(num)
+    num -= 1
