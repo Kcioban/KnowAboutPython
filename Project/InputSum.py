4421892bf02dc9e71766e92383b4361131127638
Kayky_CN <84750720+Kcioban@users.noreply.github.com>
2023-09-05 11:53:59 -0300

    Update and rename SomaEx.py to InputSum.py
    rework codigo

---
+++ Project/InputSum.py
@@ -0,0 +1,9 @@
+# Declaração de variavel
+num1 = int(input("Para realizar uma soma insira o primeiro numero: "))
+num2 = int(input("agora insira o segundo numero: "))
+
+# Soma
+soma = num1 + num2
+
+# Plot na tela
+print('A soma dos números é', soma)
