34dbd9b71e67eca456a7f4abc09bc3c55eaa88ae
Kayky_CN <84750720+Kcioban@users.noreply.github.com>
2023-09-19 15:55:17 -0300

    Create Ex01.py

---
+++ Faculdade/Aula4/Ex01.py
@@ -0,0 +1,9 @@
+# modulo de impressão
+def imprimir(nome):
+    print("Nome inserido:", nome)
+
+# Solicita que o usuário insira um nome
+nome = input("Insira o seu nome: ")
+
+# Chama a função >imprimir< 
+imprimir(nome)
