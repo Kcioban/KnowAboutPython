26a4635970074841c03504271ee2674890539df1
Kayky_CN <84750720+Kcioban@users.noreply.github.com>
2023-09-28 20:53:07 -0300

    Update ex04.py
    rewrite

--- Faculdade/Aula3/ex04.py
+++ Faculdade/Aula3/ex04.py
@@ -1,12 +1,12 @@
-def fibonacci_com_loop(n):
+def fibonacci(n):
     a, b = 0, 1
-    resultado = [a]  # Inicializamos uma lista para armazenar os resultados
-    for _ in range(1, n):  # Começamos de 1 para já incluir o primeiro número (0)
+    resultado = [a]  # Inicializa lista para armazenar os resultados
+    for _ in range(1, n):  # Começa do 1 para já incluir o primeiro número (0)
         a, b = b, a + b
-        resultado.append(a)  # Adicionamos o número atual à lista de resultados
+        resultado.append(a)  # Adiciona o número atual à lista de resultados
     return resultado
 
-# Exemplo de uso:
+# Exemplo:
 n = 10
-sequencia = fibonacci_com_loop(n)
+sequencia = fibonacci(n)
 print(f"Sequência de Fibonacci até o {n}-ésimo número: {sequencia}")
