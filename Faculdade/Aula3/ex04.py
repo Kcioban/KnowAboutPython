12ccd97d741e84b22c6ac39a6aae9a29ba510812
Kayky_CN <84750720+Kcioban@users.noreply.github.com>
2023-09-28 20:49:20 -0300

    Update ex04.py
    fibonacci

--- Faculdade/Aula3/ex04.py
+++ Faculdade/Aula3/ex04.py
@@ -1,2 +1,12 @@
-print int(1)
-print for(int(aux)+1)
+def fibonacci_com_loop(n):
+    a, b = 0, 1
+    resultado = [a]  # Inicializamos uma lista para armazenar os resultados
+    for _ in range(1, n):  # Começamos de 1 para já incluir o primeiro número (0)
+        a, b = b, a + b
+        resultado.append(a)  # Adicionamos o número atual à lista de resultados
+    return resultado
+
+# Exemplo de uso:
+n = 10
+sequencia = fibonacci_com_loop(n)
+print(f"Sequência de Fibonacci até o {n}-ésimo número: {sequencia}")
