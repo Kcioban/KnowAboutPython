614fb63b299b820d6106a15ae7d91059f6485a98
Kayky_CN <84750720+Kcioban@users.noreply.github.com>
2023-10-25 22:07:21 -0300

    Update ex01.py
    adjust indent

--- Faculdade/aula10/ex01.py
+++ Faculdade/aula10/ex01.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-
 def Fahrenheit():
     try:
         valorC = float(value_entry.get())
@@ -9,7 +8,6 @@ def Fahrenheit():
             text=f"{valorC} graus Celsius é igual a {valorF} graus Fahrenheit")
     except ValueError:
         result_label.config(text="Valor inválido")
-
 
 def Celsius():
     try:
@@ -20,7 +18,6 @@ def Celsius():
     except ValueError:
         result_label.config(text="Valor inválido")
 
-
 convertApp = tk.Tk()
 convertApp.title("Conversao de Graus")
 convertApp.resizable(False, False)
@@ -30,18 +27,15 @@ value_entry = tk.Entry(convertApp, width=10)
 value_entry.grid(row=0, column=1)
 value_entry.configure(bg="#a9b6c1")
 
-celsius_entry = tk.Button(
-    convertApp, text="Converta de C para F", command=Fahrenheit)
+celsius_entry = tk.Button(convertApp, text="Converta de C para F", command=Fahrenheit)
 celsius_entry.grid(row=1, column=0, padx=10, pady=10)
 celsius_entry.configure(bg="#a9b6c1")
 
-fahrenheit_entry = tk.Button(
-    convertApp, text="Converta de F para C", command=Celsius)
+fahrenheit_entry = tk.Button(convertApp, text="Converta de F para C", command=Celsius)
 fahrenheit_entry.grid(row=1, column=1, padx=10, pady=10)
 fahrenheit_entry.configure(bg="#a9b6c1")
 
-result_label = tk.Label(convertApp, text="", font=(
-    "Inter", 12, "bold"), foreground="#000000", background="#767b80")
+result_label = tk.Label(convertApp, text="", font=("Inter", 12, "bold"), foreground="#000000", background="#767b80")
 result_label.grid(row=2, column=0, columnspan=2, padx=10, pady=5, sticky='w')
 
 convertApp.mainloop()
