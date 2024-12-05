e67c49f437a6d67d1bdf24faa03c43798c5d42af
Kayky_CN <84750720+Kcioban@users.noreply.github.com>
2024-12-05 15:24:05 -0300

    Update main.py

--- Project/SorteadorGUI/main.py
+++ Project/SorteadorGUI/main.py
@@ -1,6 +1,27 @@
 import tkinter as tk
 root = tk.Tk()
 
+# Import da biblioteca de randomização
+import random
+
+proximo_numero = 1  # Variável global inicializada com 1
+
+def add_pessoas_lista(lista_nomes):
+    """Adiciona pessoas à lista a partir de uma lista de nomes.
+    Args:
+        lista_nomes (list): Lista de nomes a serem adicionados.
+    """
+
+    for nome in lista_nomes:
+        numero = int(numero.get())  # Assumindo que numero.get() retorna o próximo número
+        pessoa = Pessoa(nome, numero)
+        pessoas.append(pessoa)
+        messagebox.showinfo("Sucesso", "Pessoa criada com sucesso!")
+        limpar_entrada()
+
+# Lista de nomes pronta
+nomes = [“Ana Cristina Ferreira”, “Andréia Lima”, “Euclemes Junior”, “Bruno Leonardo Bertazzi”, “Caique Aparecido dos Santos”, “Claudio Henrique Fortunato”, “Daiane de Souza da Silva”, “Danilo Freire”, “Elisangela Almeida”, “Felipe Rovaris Leme”, “Fernando Henrique Bárbaro”, “Flavio Alves Leme”, “Gabriel Ferreira Francisco”, “George de Paula Cristiani”, “Heros Mello”, “Hugo Drago”, “Iasmim Rodrigues da Silva”, “Iranildo Melzani Ferreira”, “Isabela Casagrande Pazinatti”, “Jade Fernanda da Silva Luiz”, “Jose Pereira da Silva Junior”, “Juliano Divino Ramos”, “Julio Cesar Firmiano de Moraes”, “Juscelino Silva”, “Kauane Beatriz da Silva”, “Kayky Nakagawa”, “Leandro Aires de Souza”, “Leonam Barbosa”, “Luanda Almeida Antunes”, “Lucas Barbosa Ramos”, “Marcelo Domingues de Souza”, “Marco Tulio Andrade”, “Maria Eduarda Cunha”, “Maria Tereza”, “Mateus Marchesini”, “Nara Borges”, “Nicoli da Silva Oliveira”, “Paul Cleveland”, “Paulo Eiclher”, “Pedro Henrique Flores”, “Pedro Henrique Rodrigues”, “Raniel Ribeiro”, “Raphael Da Silva Araujo”, “Renato Mendes Barros”, “Rosangela Lourdes Ferreira”, “Samir Rogério Costa”, “Stefani Scareli”, “Tamires Cassia Duo Madeira”, “Vitor Hugo Serigatti”, “Vitor Tarik Colinski Pereira”, “Vivian Cristina Firmino”, “Wesley Alexandre de Carvalho”, “Wesley Oliveira”]
+
 imagem = tk.PhotoImage(file="c:/Users/Public---ALTERAR PATH---Pictures/stack-overflow.png")
 w = tk.Label(root, image=imagem)
 w.imagem = imagem
